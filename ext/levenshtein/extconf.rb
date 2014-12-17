fed1b22c5b3fb40c569b400cab1fc3362fbe20e7
Gianluca Della Vedova <gianluca@dellavedova.org>
2014-12-17 05:59:18 +0100

    C11 standard

--- ext/levenshtein/extconf.rb
+++ ext/levenshtein/extconf.rb
@@ -1,2 +1,3 @@
 require 'mkmf'
+$defs.push("-m64 -std=c11 -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes -Wmissing-prototypes -g -Wall -O3 -march=native -Wno-deprecated -Wno-parentheses -Wno-format")
 create_makefile('levenshtein')
